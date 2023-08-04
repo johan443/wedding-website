771e33e8fdb87ffc0433f0dcf0eeadf8a931129b
Johan <johan.hofmans@orphic.io>
2023-08-04 22:27:20 +0200

    Add google call

--- js/scripts.js
+++ js/scripts.js
@@ -214,11 +214,12 @@ $(document).ready(function () {
 
         $('#alert-wrapper').html(alert_markup('info', '<strong>Just a sec!</strong> We are saving your details.'));
 
-        if (MD5($('#invite_code').val()) !== 'b0e53b10c1f55ede516b240036b88f40'
-            && MD5($('#invite_code').val()) !== '2ac7f43695eb0479d5846bb38eec59cc') {
-            $('#alert-wrapper').html(alert_markup('danger', '<strong>Sorry!</strong> Your invite code is incorrect.'));
-        } else {
-            $.post('https://script.google.com/macros/s/AKfycbyo0rEknln8LedEP3bkONsfOh776IR5lFidLhJFQ6jdvRiH4dKvHZmtoIybvnxpxYr2cA/exec', data)
+        // if (MD5($('#invite_code').val()) !== 'b0e53b10c1f55ede516b240036b88f40'
+        //     && MD5($('#invite_code').val()) !== '2ac7f43695eb0479d5846bb38eec59cc') {
+        //     $('#alert-wrapper').html(alert_markup('danger', '<strong>Sorry!</strong> Your invite code is incorrect.'));
+        // } else {
+        if (true){
+            $.post('https://script.google.com/macros/s/AKfycbyc3hvpr_gcpD30EB8QiSxJWm8hlDv2y6ZxB9b_g4vKG2RFCFr2vgPa76OcgE1bqpzL/exec', data)
                 .done(function (data) {
                     console.log(data);
                     if (data.result === "error") {

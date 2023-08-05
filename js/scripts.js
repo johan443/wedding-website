ceb1f972c71334737b3336b93b56fa8780e97969
Johan <johan.hofmans@orphic.io>
2023-08-05 20:13:09 +0200

    Cleanup

--- js/scripts.js
+++ js/scripts.js
@@ -497,3 +497,34 @@ var MD5 = function (string) {
 
     return temp.toLowerCase();
 };
+
+
+// submit()
+
+// function submit() {
+//     var urlParams = new URLSearchParams(window.location.search); 
+//     if (urlParams.has("token")){
+    
+//         var myHeaders = new Headers();
+//         myHeaders.append("Accept", "application/vnd.github.v3.raw");
+//         myHeaders.append("Authorization", "token  " + 'github_pat_11AQML3VI0MhgeaXNtpPSZ_nvvozyRa2qXXjeXwKTxdHJ69woyyzSwM1ClYz9z' + urlParams.get("token"));
+//         myHeaders.append("Cookie", "_octo=GH1.1.267893443.1688309160; logged_in=no");
+    
+//         var requestOptions = {
+//         method: 'GET',
+//         headers: myHeaders,
+//         redirect: 'follow'
+//         };
+    
+//         fetch("https://api.github.com/repos/johan443/private-wedding/contents/CNAME", requestOptions)
+//         .then(response => response.text().then(text => replaceDoc(text)))
+//         .then(result => console.log(result))
+//         .catch(error => console.log('error', error));
+//     }
+  
+//       return false;
+//   }
+
+//   function replaceDoc(doc){
+//     console.log(doc)
+//   }
